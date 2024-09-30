42bd413e719e8d4658ddfe26f2c4b7a40ad823de
Mauricio Eduardo <mauricioeduardokoop@gmail.com>
2024-09-30 16:34:30 -0300

    Cria uma função para o estado de formulário completo.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let btnIniciar = document.getElementById('btnIniciar');
 let btnVoltar = document.getElementById('btnVoltar');
+let btnCompleteForm = document.getElementById('btnCompleteForm');
 let fieldContainer = document.querySelectorAll('.field-container');
 let header = document.getElementById('header');
 let count = -1;
@@ -36,4 +37,28 @@ function showFieldsForm(acionador){
 
     // Esconde o botão voltar se não possuir campo anteroior ao atual
     count >= 1 ? btnVoltar.classList.remove('hide') : btnVoltar.classList.add('hide');
+
+    // Esconde o header se o formulário estiver sido iniciado, e altera o texto do botão começar
+    if (count >= 0) {
+        header.classList.add('hide');
+        btnIniciar.textContent = "Avançar";
+
+    }else{
+        header.classList.remove('hide');
+        btnIniciar.textContent = "Começar";
+    }
+
+
+    // Se chegar no último campo executa função formulário completo
+    completeForm(count);
+}
+
+function completeForm(count) {
+    if (count >= 7) {
+        btnIniciar.classList.add('hide');
+        btnCompleteForm.classList.remove('hide');
+    }else{
+        btnIniciar.classList.remove('hide');
+        btnCompleteForm.classList.add('hide');
+    }
 }
